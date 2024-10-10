c16091d73144514e488395757cf9724366087eab
Ruben van der Linde <ruben@conduction.nl>
2024-10-10 21:34:07 +0200

    oke first dips

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -11,27 +11,26 @@ return [
 		'directory' => ['url' => '/api/directory']
 	],
 	'routes' => [
+		['name' => 'dashboard#page', 'url' => '/test', 'verb' => 'GET'],
 		['name' => 'dashboard#page', 'url' => '/', 'verb' => 'GET'],
-		['name' => 'metadata#page', 'url' => '/metadata', 'verb' => 'GET'],
-		['name' => 'publications#page', 'url' => '/publications', 'verb' => 'GET'],
-		['name' => 'publications#attachments', 'url' => '/api/publications/{id}/attachments', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'publications#attachmentsInternal', 'url' => '/api/publications/{id}/attachments/internal', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'publications#download', 'url' => '/api/publications/{id}/download', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'catalogi#page', 'url' => '/catalogi', 'verb' => 'GET'],
-		['name' => 'search#index', 'url' => '/search', 'verb' => 'GET'],
-		['name' => 'search#index', 'url' => '/api/search', 'verb' => 'GET'],
-		['name' => 'search#indexInternal', 'url' => '/api/search/internal', 'verb' => 'GET'],
-		['name' => 'search#show', 'url' => '/api/search/{id}', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'search#showInternal', 'url' => '/api/search/internal/{id}', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'search#preflighted_cors', 'url' => '/api/{path}', 'verb' => 'OPTIONS', 'requirements' => ['path' => '.+']],
-		['name' => 'themes#index', 'url' => '/search/themes', 'verb' => 'GET'],
-		['name' => 'themes#index', 'url' => '/api/search/themes', 'verb' => 'GET'],
-		['name' => 'themes#indexInternal', 'url' => '/api/themes', 'verb' => 'GET'],
-		['name' => 'themes#show', 'url' => '/api/search/themes/{id}', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'themes#showInternal', 'url' => '/api/themes/{id}', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'directory#page', 'url' => '/directory', 'verb' => 'GET'],
-		['name' => 'directory#synchronise', 'url' => '/api/directory/{id}/sync', 'verb' => 'GET', 'requirements' => ['id' => '.+']],
-		['name' => 'configuration#index', 'url' => '/configuration', 'verb' => 'GET'],
-		['name' => 'configuration#create', 'url' => '/configuration', 'verb' => 'POST']
-	],
+		//['name' => 'metadata#page', 'url' => '/metadata', 'verb' => 'GET'],
+		//['name' => 'publications#page', 'url' => '/publications', 'verb' => 'GET'],
+		//['name' => 'publications#attachments', 'url' => '/api/publications/{id}/attachments', 'verb' => 'GET'],
+		//['name' => 'publications#attachmentsInternal', 'url' => '/api/publications/{id}/attachments/internal', 'verb' => 'GET'],
+		//['name' => 'publications#download', 'url' => '/api/publications/{id}/download', 'verb' => 'GET'],
+		//['name' => 'catalogi#page', 'url' => '/catalogi', 'verb' => 'GET'],
+		//['name' => 'themes#indexInternal', 'url' => '/api/themes', 'verb' => 'GET'],
+		//['name' => 'themes#showInternal', 'url' => '/api/themes/{id}', 'verb' => 'GET'],
+		//['name' => 'directory#page', 'url' => '/directory', 'verb' => 'GET'],
+		//['name' => 'directory#synchronise', 'url' => '/api/directory/{id}/sync', 'verb' => 'GET'],
+		//['name' => 'configuration#index', 'url' => '/configuration', 'verb' => 'GET'],
+		//['name' => 'configuration#create', 'url' => '/configuration', 'verb' => 'POST']
+		// search endpoints		
+		//['name' => 'search#preflighted_cors', 'url' => '/api/search/{path}', 'verb' => 'OPTIONS', 'requirements' => ['path' => '.+']],
+		//['name' => 'search#index', 'url' => '/api/search', 'verb' => 'GET'],
+		//['name' => 'search#show', 'url' => '/api/search/publication/{id}', 'verb' => 'GET', 'requirements' => ['id' => '\d+']],
+		//['name' => 'search#show', 'url' => '/api/search/publication/{id}/attachments', 'verb' => 'GET', 'requirements' => ['id' => '\d+']],
+		//['name' => 'themes#index', 'url' => '/api/search/themes', 'verb' => 'GET'],
+		//['name' => 'themes#show', 'url' => '/api/search/themes/{id}', 'verb' => 'GET', 'requirements' => ['id' => '\d+']
+	]
 ];
